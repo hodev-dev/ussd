4b2f2dca4de86f2ed5fa87a92ef6b0bc183b15a6
ho dev <hodev.dev@gmail.com>
2021-03-05 18:03:55 +0330

    test4

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (Request $request) {
-    $mobile = $request['mobile'];
-    $call = $request['call'];
-    $sessionid = $request['sessionid'];
+Route::get('/', function () {
     return new Response('hello', 200, ['Content-Type' => 'text/plain']);
 });
