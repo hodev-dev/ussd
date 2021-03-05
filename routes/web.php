edf55270a5fc291b38933571549799bae84b1117
ho dev <hodev.dev@gmail.com>
2021-03-05 18:05:10 +0330

    test5

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return new Response('hello', 200, ['Content-Type' => 'text/plain']);
+    return response()->json(['name' => 'test']);
 });
