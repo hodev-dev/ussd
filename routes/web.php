0741756e99d4a41118867347350429a2d2872ef1
ho dev <hodev.dev@gmail.com>
2021-03-05 17:25:42 +0330

    done

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    $mobile = $request['mobile'];
+    $call = $request['call'];
+    $sessionid = $request['sessionid'];
+    return new Response('hello', 200, ['Content-Type' => 'text/plain']);
 });
